5792887cf224467a0d73b661741dbb3b84980cf1
Michael Suhendra <michael.suhendra@tum.de>
2023-03-10 20:37:00 +0100

    not working version (geometries don't show on map)

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import mapboxgl from '!mapbox-gl'; 
+//import 'mapbox-gl/dist/mapbox-gl.css';
+import * as styles from '../styles/mapbox-gl-2.3.1.css';
 import config from '../../config/config.json'
 
 // set access token from config
 mapboxgl.accessToken = config.MAPBOX_ACCESS_TOKEN;
 
-export default class App extends React.PureComponent {
+export default class App extends React.Component {
 
     constructor(props) {
         super(props);
@@ -13,18 +15,64 @@ export default class App extends React.PureComponent {
             lng: 11.56,
             lat: 48.135,
             zoom: 10,
+            currentMap: null
         };
         this.mapContainer = React.createRef();
         }
     
         componentDidMount() {
-            const { lng, lat, zoom } = this.state;
+            const { lng, lat, zoom, currentMap } = this.state;
             const map = new mapboxgl.Map({
                 container: this.mapContainer.current,
                 style: 'mapbox://styles/mapbox/streets-v12',
+                //style: styles,
                 center: [lng, lat],
-                zoom: zoom
+                zoom: zoom,
+                //scrollZoom: false
             });
+            // add fullscreen button (top-right)
+            map.addControl(new mapboxgl.FullscreenControl());
+            // add navigation buttons (top-right)
+            map.addControl(new mapboxgl.NavigationControl());
+            // disable touchscreen?
+            map.touchPitch.disable();
+            // load emission inventory maps
+            map.on('load', () => {
+                map.addSource('boundary-rect', {
+                    type: 'geojson',
+                    data: {
+                        properties: {},
+                        type: 'Feature',
+                        geometry: {
+                            type: 'Polygon',
+                            coordinates: [
+                                [
+                                    [11.0, 48.50833],
+                                    [12.2166, 48.50833],
+                                    [12.2166, 47.7],
+                                    [11.0, 47.7],
+                                    [11.0, 48.50833],
+                                ],
+                            ],
+                        },
+                    },
+                });
+                map.addLayer({
+                    id: 'boundary-rect',
+                    type: 'line',
+                    source: 'boundary-rect',
+                    layout: {},
+                    paint: {
+                        'line-color': '#1E3A8A',
+                        'line-width': 3,
+                        'line-opacity': 0.7,
+                    },
+                });
+                console.log("map is loaded") //not called
+                this.setState({currentMap: map});
+                // clean up on unmount
+                return () => this.map.remove();
+            }, []);
         }
 
         render() {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,20 @@
 import * as React from "react"
 import ReactDOM from 'react-dom';
+import { Link } from "gatsby";
 import 'mapbox-gl/dist/mapbox-gl.css';
-import App from './App';
 
+// This is the homepage
 const IndexPage = () => {
   return (
     <main>
-      <h1>TUM GNFR C Emission Inventory</h1>
+      <h1 className="text-3xl font-bold underline">TUM GNFR C Munich Emission Inventory</h1>
+      <div className="map-container">
+        <button type="button" class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+          <Link to="/App"> 
+            <h2>Open map</h2> 
+          </Link>
+        </button>
+      </div>
     </main>
   )
 }
