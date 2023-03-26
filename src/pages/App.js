b31496909184998c69c788b1534da0a28300d44a
Michael Suhendra <michael.suhendra@tum.de>
2023-03-26 23:27:11 +0200

    complete first map, add legend (TODO change data source not to take from Mapbox Studio)

--- src/pages/App.js
+++ src/pages/App.js
@@ -15,17 +15,60 @@ export default class App extends React.Component {
             lng: 11.56,
             lat: 48.135,
             zoom: 10,
-            currentMap: null
+            currentMap: null // should be the html element id of the map
         };
         this.mapContainer = React.createRef();
-        }
+    }
+
+    createMapLegend() {
+        const layers = [
+            '0-135',
+            '135-400',
+            '400-706',
+            '706-1064',
+            '1064-1582',
+            '1582-1915',
+            '1915-2248',
+            '2248-3353'
+        ];
+        const colors = [
+            '#FFF5F0',
+            '#FEE0D2',
+            '#FCBBA1',
+            '#FC9272',
+            '#FB6A4A',
+            '#EF3B2C',
+            '#CB181D',
+            '#99000D'
+        ];
+        // create legend
+        const legend = document.getElementById('legend');
+        const titleItem = document.createElement('div');
+        const titleKey = document.createElement('span');
+        titleKey.innerHTML = 'CO₂ [t]';
+        titleItem.appendChild(titleKey);
+        legend.appendChild(titleItem);
+
+        layers.forEach((layer, i) => {
+            const color = colors[i];
+            const item = document.createElement('div');
+            const key = document.createElement('span');
+            key.className = 'legend-key';
+            key.style.backgroundColor = color;
+
+            const value = document.createElement('span');
+            value.innerHTML = `${layer}`;
+            item.appendChild(key);
+            item.appendChild(value);
+            legend.appendChild(item);
+        });
+    }
     
         componentDidMount() {
             const { lng, lat, zoom, currentMap } = this.state;
             const map = new mapboxgl.Map({
                 container: this.mapContainer.current,
-                style: 'mapbox://styles/mapbox/streets-v12',
-                //style: styles,
+                style: 'mapbox://styles/michael-suhendra/clfoky47x000001qw776e7oh7',
                 center: [lng, lat],
                 zoom: zoom,
                 //scrollZoom: false
@@ -47,6 +90,14 @@ export default class App extends React.Component {
             // load emission inventory maps
             // (rectangular) border: [11.36077835, 48.06162486, 11.72290934, 48.2481186 ]
             map.on('load', () => {
+                // make a pointer cursor
+                map.getCanvas().style.cursor = 'default';
+                // create legend
+                this.createMapLegend();
+                // data goes here
+                // load all 12 maps
+
+                // rectangular boundary around Munich
                 map.addSource('boundary-rect', {
                     type: 'geojson',
                     data: {
@@ -95,6 +146,7 @@ export default class App extends React.Component {
                 Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
                 </div>
                 <div ref={this.mapContainer} className="map-container" />
+                <div class='map-overlay' id='legend'></div>
             </div>
             );
         }
